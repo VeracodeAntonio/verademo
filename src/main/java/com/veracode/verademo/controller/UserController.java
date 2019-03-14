6a1985d37df83aba67fde04414bd8d53b0d22c19
areche <areche@veracode.com>
2019-03-14 17:45:00 +0100

    mejor

--- src/main/java/com/veracode/verademo/controller/UserController.java
+++ src/main/java/com/veracode/verademo/controller/UserController.java
@@ -311,9 +311,12 @@ public class UserController {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(Constants.create().getJdbcConnectionString());
 
-			String sql = "SELECT username FROM users WHERE username = '" + username + "'";
-			Statement statement = connect.createStatement();
-			ResultSet result = statement.executeQuery(sql);
+			//MALString sql = "SELECT username FROM users WHERE username = '" + username + "'";
+			String sql = "SELECT username FROM users WHERE username = ? ";
+			//MALStatement statement = connect.createStatement();
+			//MALResultSet result = statement.executeQuery(sql);
+			PreparedStatement statement = connect.prepareStatement(sql);
+			 ResultSet result = statement.executeQuery();
 			if (result.first()) {
 				model.addAttribute("error", "Username '" + username + "' already exists!");
 				return "register";
